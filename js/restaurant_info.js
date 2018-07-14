11b84d9607bf69fa3a7ca0bff5b0d0dd47493b02
Sahil Dhawan <excelsior.sd@gmail.com>
2018-07-15 02:26:21 +0530

    responsiveness fixed

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -130,7 +130,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
   const title = document.createElement('h2');
-  title.innerHTML = 'Reviews';
+  title.innerHTML = 'REVIEWS';
   container.appendChild(title);
 
   if (!reviews) {
