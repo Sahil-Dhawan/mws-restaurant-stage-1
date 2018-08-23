eb3535ec09396ade20afdfa116bbdf9c559c9a0f
Sahil Dhawan <excelsior.sd@gmail.com>
2018-08-23 05:48:48 +0530

    service worker updated to cache images and pages

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
-var staticCacheName = 'restaurantsw-static-v1';
+let staticCacheName = 'restaurantsw-static-v1';
+let contentImageCacheName = 'restaurantsw-image-static-v1';
+let contentPageCacheName = 'restaurantsw-page-static-v1';
+let allCaches = [
+  staticCacheName,
+  contentImageCacheName,
+  contentPageCacheName
+];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
@@ -10,9 +17,11 @@ self.addEventListener('install', function(event) {
         'js/main.js',
         'js/restaurant_info.js',
 				'js/dbhelper.js',
+        'js/idb.js',
+        'sw.js',
 				'css/styles.css',
         'css/mediaqueries.css',
-				'data/restaurants.json'
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
       ]);
     })
   );
@@ -24,7 +33,7 @@ self.addEventListener('activate', function(event) {
       return Promise.all(
         cacheNames.filter(function(cacheName) {
           return cacheName.startsWith('restaurantsw-') &&
-                 cacheName != staticCacheName;
+                 !allCaches.includes(cacheName);
         }).map(function(cacheName) {
           return caches.delete(cacheName);
         })
@@ -41,6 +50,17 @@ self.addEventListener('fetch', function(event) {
       event.respondWith(caches.match('/index.html'));
       return;
     }
+    if (requestUrl.pathname.endsWith('.jpg')||requestUrl.pathname.endsWith('.png')) {
+    //  console.log("in img endswith")
+      event.respondWith(servePhoto(event.request));
+      return;
+    }
+    if (event.request.url.includes('.html')){
+      event.respondWith(servePage(event.request));
+      return;
+    }
+
+
   }
 
   event.respondWith(
@@ -49,6 +69,40 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
+function servePage(request) {
+  let storageUrl =request.url;
+
+  return caches.open(contentPageCacheName).then(function(cache) {
+    return cache.match(storageUrl).then(function(response) {
+      if (response) return response;
+
+      return fetch(request).then(function(networkResponse) {
+        cache.put(storageUrl, networkResponse.clone());
+        return networkResponse;
+      });
+    });
+  });
+}
+
+function servePhoto(request) {
+  let storageUrl =request.url;
+  if(request.url.includes('_1x')){
+  storageUrl = request.url.replace('1x','2x');
+}
+  //storageUrl = request.url.replace(/_\d+x\.jpg$/, '');
+
+  return caches.open(contentImageCacheName).then(function(cache) {
+    return cache.match(storageUrl).then(function(response) {
+      if (response) return response;
+
+      return fetch(request).then(function(networkResponse) {
+        cache.put(storageUrl, networkResponse.clone());
+        return networkResponse;
+      });
+    });
+  });
+}
+
 
 self.addEventListener('message', function(event) {
   if (event.data.action === 'skipWaiting') {
